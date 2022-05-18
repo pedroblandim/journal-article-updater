dc65ca8831325ed1c88f3aef3e11236c4678a908
pedroblandim <pedro.blandim@gmail.com>
2022-05-18 11:45:47 -0300

    Adjusts for UAT

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ import urllib.parse as urlparse
 from urllib.parse import urlencode
 
 
-timeout = 2
-delta = 200
-
+timeout = 7
+delta = 2
+initial_result = 0
+inital_page = 0
 
 def run():
     url = set_query_params(os.environ['url'])
@@ -24,25 +25,35 @@ def run():
 
     login()
 
-    assert_in_legislacoes_page()
-
     # update all legislacoes from page
     while(True):
-        i = 0
+        i = initial_result
+        legislacoes = []
         while(True):
+            
+            if len(legislacoes) == 0:
+                legislacoes = find_elements(
+                    By.CSS_SELECTOR, '#_com_liferay_journal_web_portlet_JournalPortlet_articlesSearchContainer tr.entry-display-style')
 
-            assert_in_legislacoes_page()
-            legislacoes = find_elements(
-                By.CSS_SELECTOR, '#_com_liferay_journal_web_portlet_JournalPortlet_articlesSearchContainer > div.table-responsive > table > tbody > tr.entry-display-style')
+            if not legislacoes:
+                add_cur_iteration_to_error_file(i)
+                continue
 
             if (i >= len(legislacoes)):
                 break
 
-            update_legislacao(legislacoes[i])
+            legislacao = legislacoes[i]
+            
+            legislacao_name = legislacao.find_element(By.CSS_SELECTOR, '.table-cell-expand.table-cell-minw-200.table-title.lfr-title-column > a').text
+            words_to_check = ['nº', 'Nº', 'n.º', 'N.º', 'N.', 'n.', 'no']
+            if not any(word in legislacao_name for word in words_to_check):
+                i += 1
+                continue
 
+            update_legislacao(legislacao)
+
+            legislacoes = []
             i += 1
-        
-        assert_in_legislacoes_page()
 
         next_page_button = find_element(By.CSS_SELECTOR, '#_com_liferay_journal_web_portlet_JournalPortlet_articlesPageIteratorBottom > ul > li:last-child')
         if not 'disabled' in next_page_button.get_attribute('class'):
@@ -80,8 +91,6 @@ def update_legislacao(legislacao):
     # other element making it not clickable
     driver.execute_script("arguments[0].click();", legislacao_link)
 
-    assertInLegislacaoPage()
-
     int_numero_input = find_element(
         By.CSS_SELECTOR, '[id^="_com_liferay_journal_web_portlet_JournalPortlet_numeroINT_INSTANCE_"]')
     numero_input = find_element(
@@ -101,17 +110,27 @@ def update_legislacao(legislacao):
     # update legislacao
     int_numero_input.clear()
     int_numero_input.send_keys(int(numero))
-
+    
     # save legislacao
-    find_element(
-        By.ID, '_com_liferay_journal_web_portlet_JournalPortlet_publishButton').click()
+    save_button = find_element(By.ID, '_com_liferay_journal_web_portlet_JournalPortlet_publishButton')
+    save_button.click()
 
-    error_block = find_element(
-        By.CSS_SELECTOR, 'div.form-validator-stack.help-block')
+    try:
+        error_block = driver.find_element(
+            By.CSS_SELECTOR, 'div.form-validator-stack.help-block')
 
-    if error_block:
-        add_cur_legislacao_to_error_file()
-        go_back_button.click()
+        if error_block:
+            add_cur_legislacao_to_error_file()
+            go_back_button.click()
+    except:
+        try:
+            # still on edit page
+            submit_button = driver.find_element(By.ID, '_com_liferay_journal_web_portlet_JournalPortlet_publishButton')
+            if submit_button:
+                submit_button.click()
+            return
+        except:
+            return
 
 
 def add_cur_legislacao_to_error_file():
@@ -130,6 +149,24 @@ def add_cur_legislacao_to_error_file():
 
     with open(file_name, 'w') as f:
         f.write(str(my_set))
+
+def add_cur_iteration_to_error_file(i):
+    file_name = 'legislacoes-not-updated-' + os.environ['env'] + '.txt'
+
+    Path(file_name).touch(exist_ok=True)  # creates file if it doesn't exist
+    with open(file_name, 'r') as f:
+        names = f.read()
+
+    my_set = set() if not names else ast.literal_eval(names)
+
+    page = find_element(
+        By.CSS_SELECTOR, '#_com_liferay_journal_web_portlet_JournalPortlet_articlesPageIteratorBottom li.active.page-item').text
+
+    my_set.add('page ' + page + ' result ' + i)
+
+    with open(file_name, 'w') as f:
+        f.write(str(my_set))
+
 
 
 def find_element(by, value):
@@ -152,15 +189,25 @@ def find_elements(by, value):
     try:
         element_present = EC.presence_of_element_located((by, value))
         WebDriverWait(driver, timeout).until(element_present)
+        
+        try:
+            elements = driver.find_elements(by, value)
+            len(elements) > 0 # validate element
+            return elements
+        
+        except: # try again
+            print('error')
+            return driver.find_elements(by, value)
 
-        return driver.find_elements(by, value)
     except TimeoutException:
         print("Timed out waiting for page to load")
 
 
 def set_query_params(url):
     params = {'_com_liferay_journal_web_portlet_JournalPortlet_delta': delta,
-              '_com_liferay_journal_web_portlet_JournalPortlet_orderByCol': 'title'}
+              '_com_liferay_journal_web_portlet_JournalPortlet_orderByCol': 'title',
+              '_com_liferay_journal_web_portlet_JournalPortlet_displayStyle': 'list',
+              '_com_liferay_journal_web_portlet_JournalPortlet_cur': inital_page}
 
     url_parts = list(urlparse.urlparse(url))
     query = dict(urlparse.parse_qsl(url_parts[4]))
@@ -169,18 +216,6 @@ def set_query_params(url):
     url_parts[4] = urlencode(query)
 
     return urlparse.urlunparse(url_parts)
-
-
-def assert_in_legislacoes_page():
-    title_elem = find_element(
-        By.CSS_SELECTOR, '#_com_liferay_journal_web_portlet_JournalPortlet_controlMenu > li.control-menu-nav-category.tools-control-group > ul > li > span')
-    assert title_elem.text == "Legislações"
-
-
-def assertInLegislacaoPage():
-    article_structure = find_element(
-        By.CSS_SELECTOR, '#basicInformationContent > div > p.article-structure')
-    assert article_structure.text == 'Estrutura: Legislações'
 
 
 driver = webdriver.Chrome()
